e2452f24cae3b5cf1259da8e7f1bc2077879a46b
Dhaval Rupapara <rupaparadhaval1234@gmail.com>
2024-01-16 23:09:28 +0530

    updated components

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Header from './components/Header/Header.jsx'
+import { RouterProvider } from 'react-router-dom'
+
+const router=
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider />
   </React.StrictMode>,
 )
